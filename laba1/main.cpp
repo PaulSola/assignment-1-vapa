0218d42833b511ca6ed4fae21644d06386b5919d
ITrokh <trohimenko.99@ukr.net>
2017-04-03 17:47:32 +0300

    final

--- laba1/main.cpp
+++ laba1/main.cpp
@@ -18,15 +18,23 @@ struct Team {
 void INter(struct Team mass[22], struct Devider num[22]);
 void Middle(struct Team mass[22]);
 
-int main()
+int main(int argc, char* argv[])
 {
+	if (argc > 1)// если передаем аргументы, то argc будет больше 1(в зависимости от кол-ва аргументов)
+	{
+		cout << argv[1] << endl;// вывод второй строки из массива указателей на строки(нумерация в строках начинается с 0 )
+	}
+	else
+	{
+		cout << "Not arguments" << endl;
+	}
 	setlocale(LC_ALL, "Russian");
 	struct Devider arg[22];
 	struct Team arn[22];
 	INter(arn, arg);
 	Middle(arn);
 	system("pause");
-
+	return 0;
 }
 void INter(struct Team mass[22], struct Devider num[22])
 {
